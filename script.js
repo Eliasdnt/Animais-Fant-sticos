f068d15240bb590df7d980c1facb0dc5b117b6bd
antonio elias <antonio.elias71@aluno.ifce.edu.br>
2024-04-17 10:06:27 -0300

    .

--- script.js
+++ script.js
@@ -35,3 +35,28 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+function scrollSuave() {
+    const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+    console.log(linksInternos);
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href'); // Fixed typo in currentTarget
+        console.log(href);
+        const section = document.querySelector(href);
+
+        if (section) { // Added a check if section exists
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection); // Added scrollToSection as event listener
+    });
+}
+
+scrollSuave();
